61d15e9cef9bef4646b65a1884a4b49b3be1372d
Sudeepa Noble <sudeepa.kolli@gmail.com>
2021-08-03 23:23:55 +0530

    pr

--- src/images.js
+++ src/images.js
@@ -1,4 +1,4 @@
-import lead2021 from './assests/images/8F2E218B6F8EBA330DFDD3146BDC48C1.jpg';
+import lead2021 from './assests/images/6F16E7CEE9F1260A83E51FDDA1B81C97.jpg';
 export const images = {
-    "8F2E218B6F8EBA330DFDD3146BDC48C1": lead2021
+    "6F16E7CEE9F1260A83E51FDDA1B81C97": pr_cf
 }
