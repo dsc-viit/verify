8eb22fef3d723d3f55be18643607a4dfc6d6beaf
akhileshkoti <akhileshkoti2001@gmail.com>
2021-08-04 10:01:14 +0530

    updated

--- src/images.js
+++ src/images.js
@@ -1,5 +1,6 @@
 import lead2021 from './assests/images/8F2E218B6F8EBA330DFDD3146BDC48C1.jpg';
+import akhil_1201 from './assests/images/4kh!l.jpg';
 export const images = {
     "8F2E218B6F8EBA330DFDD3146BDC48C1": lead2021,
-    "5A2AED19A11DE3CB10C45DF1F3047C99": '4kh!l'
+    "5A2AED19A11DE3CB10C45DF1F3047C99": akhil_1201
 }
