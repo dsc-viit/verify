a3ac9ee06331f19fadb2daddf62795fad7733e96
eswar2001 <teswar2001@gmail.com>
2021-08-04 23:07:58 +0530

    pr merged

--- src/images.js
+++ src/images.js
@@ -1,5 +1,5 @@
 import lead2021 from "./assests/images/8F2E218B6F8EBA330DFDD3146BDC48C1.jpg";
-import ronin from "./assests/images/83BC9FE97429A693FD86AEE66B22D254.jpeg";
+import ronin from "./assests/images/83BC9FE97429A693FD86AEE66B22D254.jpg";
 import pr_cf from "./assests/images/6F16E7CEE9F1260A83E51FDDA1B81C97.jpg";
 import akhil_1201 from "./assests/images/5A2AED19A11DE3CB10C45DF1F3047C99.jpeg";
 import co_lead21 from "./assests/images/6F113046D7F8DF1AECAED3F2E9ED4126.jpg";
